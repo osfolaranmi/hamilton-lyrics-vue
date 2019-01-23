ad28e9d1096fa5129561e4b1da4e3c443ed7860f
Oluwatosin Folaranmi <foluwatosin@ITILP_L014.simplexsystems.com>
2019-01-23 19:14:09 +0100

    Initial commit

---
+++ src/components/ChannelDetails.vue
@@ -0,0 +1,27 @@
+<script>
+  import Pusher from 'pusher-js'
+  const pusher = new Pusher('80493e20ea0f59dafb82', {
+    cluster: 'mt1',
+    encrypted: true,
+    authEndpoint: 'http://localhost:5000/pusher/auth'
+  })
+
+  export default ({
+    getPresenceID () {
+      let getQueryString = (field, url) => {
+        let href = url ? url : window.location.href
+        let reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i')
+        let string = reg.exec(href)
+        return string ? string[1] : null
+      }
+      let id = getQueryString('id')
+      id = 'presence-' + id
+      return id
+    },
+    subscribeToPusher () {
+      let presenceid = this.getPresenceID()
+      let channel = pusher.subscribe(presenceid)
+      return channel
+    }
+  })
+</script>
